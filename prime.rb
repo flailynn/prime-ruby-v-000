06e1440387ea122eaf7b3c1f50cdc8e9a62c826a
Anita Beauchamp <amadamspr@gmail.com>
2018-07-22 22:14:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(number)
     false
   else
 
-    factor_array = (2..number).select do |factor|
+    factor_array = (1..number).select do |factor|
       if number % factor == 0
         factor
       end
