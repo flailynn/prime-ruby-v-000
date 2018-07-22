20a4049ca945321b31e93badb815a1947f56f136
Anita Beauchamp <amadamspr@gmail.com>
2018-07-22 22:10:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  if number <= 1
+    false
+  else
+
+    factor_array = (2..number).collect do |factor|
+      factor if number % factor == 0
+    end
+    factor_array.size > 2 ? false : true
+  end
+=begin
+  elsif number == 2 || number == 3
+    true
+  elsif (number - 1) % 6 == 0
+    true
+  elsif (number + 1) % 6 == 0
+    true
+  else
+    false
+  end
+=end
+end
+
+prime?(7)
